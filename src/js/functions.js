3bd4117ab6260307c04b6a00a4d79dc30b47b8d2
HomePC <ira.intertech@gmail.com>
2016-04-25 10:52:14 +0300

    git init

---
+++ src/js/functions.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function() {
+
+    //form validate
+    jQuery("form").validate({
+
+        rules:{
+            name:{
+                required: true,
+                minlength: 2
+            },
+            phone:{
+                required: true,
+                digits: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            address:{
+                required: true
+
+            }
+
+
+        }
+
+    });
+
+
+
+
+
+
+
+    jQuery(window).load(function() {
+
+
+
+
+
+
+
+
+    });
+
+
+
+
+});
+
+
+
+
+
+
