fb73cd7626fad488f0cd77c7d55224c312384dc5
HomePC <ira.intertech@gmail.com>
2016-04-26 17:54:32 +0300

    page collections

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -84,7 +84,22 @@ jQuery(document).ready(function() {
 
 });
 
+window.onload = function(){
 
+    jQuery('.gallery').each(function () {
+        this.onclick = function (event) {
+            event = event || window.event;
+            var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {index: link, event: event},
+                links = this.getElementsByTagName('a');
+            blueimp.Gallery(links, options);
+        };
+    });
+
+
+
+};
 
 
 
