55d51425f8dd261ec70e1ea147bedad32c802bf6
ira <bakum21@ukr.net>
2016-05-26 12:41:50 +0300

    fixed mobile

--- src/js/functions.js
+++ src/js/functions.js
@@ -454,4 +454,55 @@ jQuery(document).ready(function($){
     $('.product .content > .close').click(function () {
         location.href = '/products';
     });
+
+
+
+
+
+
+
+
+
+    document.ontouchmove = function ( event ) {
+
+        var isTouchMoveAllowed = true, target = event.target;
+
+        while ( target !== null ) {
+            if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
+                isTouchMoveAllowed = false;
+                break;
+            }
+            target = target.parentNode;
+        }
+
+        if ( !isTouchMoveAllowed ) {
+            event.preventDefault();
+        }
+
+    };
+
+    function removeIOSRubberEffect( element ) {
+
+        element.addEventListener( "touchstart", function () {
+
+            var top = element.scrollTop, totalScroll = element.scrollHeight, currentScroll = top + element.offsetHeight;
+
+            if ( top === 0 ) {
+                element.scrollTop = 1;
+            } else if ( currentScroll === totalScroll ) {
+                element.scrollTop = top - 1;
+            }
+
+        } );
+
+    }
+
+    removeIOSRubberEffect( document.querySelector( ".scrollable" ) );
+
+
+
+
+
+
+
 });
